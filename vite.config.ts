a2941cbad0cfa117872f8833f46a404a6a4f24aa
rizary <andika.riyan@gmail.com>
2021-09-30 16:08:00 +0700

    remove commonjs plugin to make it works

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
+// import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import path from 'path';
 import { defineConfig } from 'vite';
@@ -8,13 +8,16 @@ const projectRootDir = path.resolve(__dirname);
 export default defineConfig({
     plugins: [
         reactRefresh(),
-        viteCommonjs(),
+        // viteCommonjs(),
     ],
     build: {
         outDir: 'dist',
         minify: false,
     },
     server: {
-        port: 3000,
+        hmr: {
+            host: 'localhost',
+            port: 3000,
+        }
     },
 })
